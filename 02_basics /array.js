83d939594c4d2b0977dcf80388adaf132dcea040
Akash Agrawal <akashji93685@gmail.com>
2025-03-31 17:52:25 +0000

    Array

---
+++ 02_basics /array.js
@@ -0,0 +1,17 @@
+const arr = [1,2,3,4]
+// console.log(arr[0]);
+// arr.push(99)
+// arr.pop();
+// arr.unshift(9)
+// arr.shift()
+// const newArr= arr.join()
+// console.log(newArr);
+
+// console.log(arr);
+
+const newarr = arr.slice(1,3)
+console.log(newarr);
+console.log(arr);
+const newarr1= arr.splice(1,2) //splice array ko manuplate kr skta hai usme se wo jo index donge wo nikal ke khtm kr dega 
+console.log(newarr1);
+console.log(arr);

f68b3342cac0ed4ffa5122b2741c884b337bfe59
Akash Agrawal <akashji93685@gmail.com>
2025-03-31 18:09:21 +0000

    Array part2

---
+++ 02_basics /02_array.js
@@ -0,0 +1,13 @@
+const marvelHeros = ["thor ","Ironman","SpiderMan"]
+const dcHeros =["superMan ","Flash ","Batman"]
+// marvelHeros.push(dcHeros)
+// const Allheros = marvelHeros.concat(dcHeros)
+const All_heros =[...dcHeros,...marvelHeros]
+console.log(All_heros);
+let s1 =100
+let s2 =200
+let s3 =300
+console.log(Array.of(s1,s2,s3));
+
+
+

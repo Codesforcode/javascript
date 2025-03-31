5d70ac7af93a68e16a86e6810728cdcf150c6961
Akash Agrawal <akashji93685@gmail.com>
2025-03-31 12:42:22 +0000

    datesAndTimes

---
+++ 01_basics/dates.js
@@ -0,0 +1,22 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+
+// let myCreatedDate =new Date(2025,0,12)
+// console.log( myCreatedDate.toDateString());
+
+
+// let date = new Date("11-01-2025")
+// console.log(date.toDateString());
+
+let myTimeStamp= Date.now();
+console.log(myTimeStamp);
+console.log(myDate.getTime());
+
+
+

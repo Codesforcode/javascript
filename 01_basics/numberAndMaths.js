2c5e148e8c8f56f45546037fb2629eae5ab95f5e
Akash Agrawal <akashji93685@gmail.com>
2025-03-31 12:22:49 +0000

    nums And Math

---
+++ 01_basics/numberAndMaths.js
@@ -0,0 +1,21 @@
+// const num=100
+// console.log(num);
+//  const score = new Number(100);
+//  console.log(score);
+//  console.log(typeof score);
+ 
+
+const num= 123.99098
+console.log(num.toFixed(2));
+console.log(num.toPrecision(3));
+console.log(Math.max(10,20,21.8,21));
+
+
+
+ 
+
+
+
+const Min= 10
+const Max=20;
+// console.log(Math.floor(Math.random()*(Max-Min +1))+Min);

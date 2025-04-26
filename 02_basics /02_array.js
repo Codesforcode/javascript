4570a99ef295ae7e6158468fad4c26c22171c605
Akash Agrawal <akashji93685@gmail.com>
2025-04-26 14:28:32 +0000

    defining Arrays

--- 02_basics /02_array.js
+++ 02_basics /02_array.js
@@ -1,13 +1,30 @@
 const marvelHeros = ["thor ","Ironman","SpiderMan"]
 const dcHeros =["superMan ","Flash ","Batman"]
-// marvelHeros.push(dcHeros)
-// const Allheros = marvelHeros.concat(dcHeros)
-const All_heros =[...dcHeros,...marvelHeros]
-console.log(All_heros);
+// marvelHeros.push(dcHeros)//make dcHeros as forth element
+// const Allheros = marvelHeros.concat(dcHeros) //concat the arrays
+// const All_heros =[...dcHeros,...marvelHeros] //spread operator
+// console.log(All_heros);
+
+
+
+
+
+// const n1 = [1,2,3,[4,5,6],9,0,[3,4,[5,6]]];
+// console.log(n1.flat(Infinity));
+// output=[
+//     1, 2, 3, 4, 5,
+//     6, 9, 0, 3, 4,
+//     5, 6
+//   ]
+
+
+
+// console.log(Array.isArray("Akash"));//returns booelan value
+// console.log(Array.from("Akash"));//convert the input in to an Array
+// console.log(Array.from({name:"Akash"}));//returns An empty Array;
 let s1 =100
 let s2 =200
 let s3 =300
 console.log(Array.of(s1,s2,s3));
-
 
 

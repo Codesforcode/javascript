a195b490cd23ccd3873da77624e4b7d419ef3c21
Akash Agrawal <akashji93685@gmail.com>
2025-04-27 19:52:47 +0000

    something more than objects

---
+++ 02_basics /04_objectt.js
@@ -0,0 +1,23 @@
+const tinderUser={}
+tinderUser.id = "Akash0123";
+tinderUser.name ="Akash";
+tinderUser.age=18;
+// console.log(tinderUser);
+const user={
+    name:{
+        fullname:{
+            firstName:"Akash",
+            lastName:"Agrawal"
+        }
+    }
+}
+// console.log(user.name.fullname.lastName);
+const u1 = [
+    {
+        name:"Akash"
+    },
+    {
+        Age:18
+    }
+]
+console.log(u1[0].name);
